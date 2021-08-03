622acdfc8f8809744a0bb948bc8c7d682f6d742d
Airplane <letadllo@gmail.com>
2021-08-03 15:52:05 +0500

    Titles, autocomplete text field, preloading idea

--- app/src/main/java/com/example/aniquiz/MusicQuizActivity.kt
+++ app/src/main/java/com/example/aniquiz/MusicQuizActivity.kt
@@ -4,11 +4,17 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Chronometer
 import android.widget.Toast
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.SimpleExoPlayer
+import kotlinx.android.synthetic.main.activity_aniquestion.*
 import kotlinx.android.synthetic.main.activity_music_quiz.*
+import kotlinx.android.synthetic.main.activity_quiz_setup.*
 import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.Main
 
@@ -27,7 +33,13 @@ class MusicQuizActivity : AppCompatActivity()
 
         preferences = getSharedPreferences(applicationContext.packageName + "_preferences", Context.MODE_PRIVATE)
 
-        exoPlayer = SimpleExoPlayer.Builder(applicationContext).build()
+        var adapter = ArrayAdapter<String>(this, R.layout.spinner_item, Globals.aniNames.values.toList())
+        adapter.setDropDownViewResource(R.layout.spinner_dropdown_item)
+        actv_title.setAdapter(adapter)
+
+        exoPlayer = SimpleExoPlayer.Builder(applicationContext).build().also { exoPlayer ->
+            pv_video.player = exoPlayer
+        }
         exoPlayer?.playWhenReady = false
 
         exoPlayer?.addListener(object : Player.Listener
@@ -39,8 +51,42 @@ class MusicQuizActivity : AppCompatActivity()
                 {
                     Player.STATE_READY ->
                     {
-                        //Toast.makeText(applicationContext, "Playing", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(applicationContext, "Playing", Toast.LENGTH_SHORT).show()
+                        //pv_video.visibility = View.INVISIBLE
                         exoPlayer?.play()
+                        cm_answertimer.isCountDown = true
+                        cm_answertimer.base = SystemClock.elapsedRealtime() + 15000
+                        cm_answertimer.onChronometerTickListener = Chronometer.OnChronometerTickListener { chronometer ->
+                            if (chronometer.base <= SystemClock.elapsedRealtime() || exoPlayer == null || !exoPlayer!!.isPlaying)
+                            {
+                                chronometer.stop()
+
+                                val exoPlayer2 = SimpleExoPlayer.Builder(applicationContext).build()
+                                exoPlayer2.playWhenReady = false
+                                exoPlayer2.setMediaItem(MediaItem.fromUri("https://animethemes.moe/video/AkuNoHana-OP1.webm"))
+
+                                exoPlayer2.addListener(object : Player.Listener
+                                {
+                                    override fun onPlaybackStateChanged(state: Int)
+                                    {
+                                        super.onPlaybackStateChanged(state)
+                                        when (state)
+                                        {
+                                            Player.STATE_READY ->
+                                            {
+                                                Toast.makeText(applicationContext, "Next", Toast.LENGTH_SHORT).show()
+                                                pv_video.player = exoPlayer2
+                                                exoPlayer2.play()
+                                                exoPlayer?.release()
+                                            }
+                                        }
+                                    }
+                                })
+                                exoPlayer2.prepare()
+                                //pv_video.visibility = View.VISIBLE
+                            }
+                        }
+                        cm_answertimer.start()
                     }
                     Player.STATE_IDLE ->
                     {
@@ -54,12 +100,26 @@ class MusicQuizActivity : AppCompatActivity()
         tv_title.text = Globals.aniNames[aniID]
 
         CoroutineScope(Main).launch {
-            val link = mediaAPI?.getThemeAudio(aniID) ?: mediaAPI?.getThemeVideo(aniID) ?: "https://animethemes.moe/video/AkuNoHana-OP1.webm"
+            //val link = mediaAPI?.getThemeAudio(aniID) ?: mediaAPI?.getThemeVideo(aniID) ?: "https://animethemes.moe/video/AkuNoHana-OP1.webm"
+            val link = mediaAPI?.getThemeVideo(aniID) ?: "https://animethemes.moe/video/AkuNoHana-OP1.webm"
+            exoPlayer?.addMediaItem(MediaItem.fromUri("https://animethemes.moe/video/AkuNoHana-OP1.webm"))
+            withContext(Dispatchers.Main)
+            {
+                Toast.makeText(applicationContext, "Got link: " + link, Toast.LENGTH_SHORT).show()
+            }
             withContext(Dispatchers.Main)
             {
                 exoPlayer!!.setMediaItem(MediaItem.fromUri(link))
                 exoPlayer!!.prepare()
             }
         }
+    }
+
+    override fun onStop()
+    {
+        super.onStop()
+        Toast.makeText(applicationContext, "Stop", Toast.LENGTH_SHORT).show()
+        exoPlayer?.release()
+        finish()
     }
 }
